dbe1a8f56faae114e4f4275e1319b3858d3d3427
yu010101 <yuichiyoshida@radineer.com>
2025-03-10 10:45:29 +0900

    feat: メタデータとfaviconを追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,18 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "台湾日本Radio Express オンラインサロン",
   description: "日台をつなぐ架け橋へ──台湾日本Radio Express オンラインサロンでしか体験できないクロスカルチャーの世界",
+  icons: {
+    icon: '/favicon.ico',
+  },
+  metadataBase: new URL('https://radineer.github.io/taiwan_radio_lp'),
+  openGraph: {
+    title: '台湾日本Radio Express オンラインサロン',
+    description: '日台をつなぐ架け橋へ──台湾日本Radio Express オンラインサロンでしか体験できないクロスカルチャーの世界',
+    url: 'https://radineer.github.io/taiwan_radio_lp',
+    siteName: '台湾日本Radio Express オンラインサロン',
+    locale: 'ja_JP',
+    type: 'website',
+  },
 };
 
 export default function RootLayout({
@@ -16,6 +28,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ja">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
       <body className={inter.className}>
         <main className="min-h-screen bg-white">
           {children}
