3b50be0076b291ba9492a68c1b93167017475034
y-yosh1da <yc-yoshida@yukoyuko.co.jp>
2025-09-01 18:32:19 +0900

    chore: GH Pages workflow uses GITHUB_PAGES=true; Vercel settings co-exist

--- next.config.js
+++ next.config.js
@@ -1,15 +1,10 @@
 /** @type {import('next').NextConfig} */
 const path = require('path')
 
+const isGhPages = process.env.GITHUB_PAGES === 'true'
+
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: '/taiwan_radio_lp',
-  images: {
-    unoptimized: true,
-    loader: 'custom',
-    loaderFile: './image-loader.js'
-  },
-  assetPrefix: '/taiwan_radio_lp/',
   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
@@ -19,4 +14,15 @@ const nextConfig = {
   }
 }
 
-module.exports = nextConfig 
+if (isGhPages) {
+  nextConfig.output = 'export'
+  nextConfig.basePath = '/taiwan_radio_lp'
+  nextConfig.assetPrefix = '/taiwan_radio_lp/'
+  nextConfig.images = {
+    unoptimized: true,
+    loader: 'custom',
+    loaderFile: './image-loader.js'
+  }
+}
+
+module.exports = nextConfig
